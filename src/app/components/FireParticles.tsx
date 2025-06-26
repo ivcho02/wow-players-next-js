b06bc38f2800b867deabdacd20b55297a914ae13
Ivaylo Tsvetanov <itsvetanov.dev@gmail.com>
2025-06-27 00:22:07 +0300

    Move particles config file to a specific folder

--- src/app/components/FireParticles.tsx
+++ src/app/components/FireParticles.tsx
@@ -1,36 +1,36 @@
 'use client'
 
 import React, { useState, useEffect, useCallback } from "react";
-import Particles, { initParticlesEngine} from "@tsparticles/react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { Container, IOptions, RecursivePartial } from "@tsparticles/engine";
-import particles from "@/app/components/particles.json";
+import particles from "@/configs/particles.json";
 import { tsParticles } from "@tsparticles/engine";
 import { loadFirePreset } from "@tsparticles/preset-fire";
 
 function FireParticles() {
-	const [init, setInit] = useState(false);
+  const [init, setInit] = useState(false);
 
-	useEffect(() => {
-		initParticlesEngine(async () => {
-			await loadFirePreset(tsParticles);
-		}).then(() => {
-			setInit(true);
-		});
-	}, []);
+  useEffect(() => {
+    initParticlesEngine(async () => {
+      await loadFirePreset(tsParticles);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
 
-	const particlesLoaded = useCallback(() => {}, []);
+  const particlesLoaded = useCallback(() => {}, []);
 
-	return (
-		<div>
-			{
-            init && 
-            <Particles 
-                id="tsparticles" 
-                particlesLoaded={particlesLoaded as (container?: Container | undefined) => Promise<void>} 
-                options={particles as RecursivePartial<IOptions>} 
-            />} 
-		</div>
-	);
+  return (
+    <div>
+      {
+        init &&
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded as (container?: Container | undefined) => Promise<void>}
+          options={particles as RecursivePartial<IOptions>}
+        />}
+    </div>
+  );
 }
 
 export default React.memo(FireParticles); 
