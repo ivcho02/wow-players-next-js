9cbfcdaeb3e93462f7b9a4261e3c59ac6fce7e1e
Ivaylo Tsvetanov <itsvetanov.dev@gmail.com>
2025-06-27 00:21:09 +0300

    Add image logo on the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import FireParticles from "./components/FireParticles";
 import CharacterDisplay from "./components/CharacterDisplay";
 import SearchForm from "./components/SearchForm";
 import { CharacterProfile } from "@/types/index";
+import Image from "next/image";
 
 export default function Home() {
   const [characterData, setCharacterData] = useState<CharacterProfile | null>(null);
@@ -21,6 +22,15 @@ export default function Home() {
 
       <div className="wow__search__container">
         <div className="wow__search__content">
+          <div className="wow__search__logo">
+            <Image
+              src="/wow-logo.png"
+              alt="World of Warcraft Logo"
+              priority
+              width={100}
+              height={100}
+            />
+          </div>
           <h1 className="wow__search__title">World of Warcraft Players</h1>
           <SearchForm onSearch={handleSearchResult} />
         </div>
